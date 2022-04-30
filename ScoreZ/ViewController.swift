b94187488292fa1cb48096446340ab33bf28cf3d
Dominic Severo <dom.severo2@gmail.com>
2022-04-30 16:50:40 -0400

    Initial Commit

---
+++ ScoreZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreZ
+//
+//  Created by Dominic Severo on 4/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
